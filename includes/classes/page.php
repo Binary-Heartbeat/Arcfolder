133f2d082d8310e24ab7ee0d5afe058c5fbd4189
Jekotia <jekotia@jekotia.net>
2013-01-01 22:45:43 -0500

    Tidying of includes folder. Moved some things.

---
+++ includes/classes/page.php
@@ -0,0 +1,41 @@
+<?php
+    class page
+    {
+        public static function invoke($_, $auth, $authLoc)
+        {
+            if (isset($_GET['page'])) {
+                $page['page']    = $_GET['page'];
+                $page['pages']   = array('auth', 'home', 'addons', 'authors', 'help', 'about', 'melder');
+                $page['actions'] = array('activate', 'register', 'login', 'logout');
+
+                if (isset($_GET['action'])) {
+                    $page['action'] = $_GET['action'];
+                } else {
+                    $page['action'] = null;
+                }
+
+                if (!in_array($page['page'], $page['pages'])) {
+                    $page['page'] = '404';
+                    $page['type'] = 'error';
+
+                } elseif ($page['page'] == 'auth' and $page['action'] !== null) {
+                    if (!in_array($page['action'], $page['actions'])) {
+                        $page['type'] = 'error';
+                        $page['page'] = '404';
+                    } else {
+                        $page['type'] = 'auth';
+                    }
+
+                } else {
+                    $page['type'] = 'page';
+                }
+
+            } else {
+                header('Location: '.$_['web_root'].'home');
+            }
+
+            // new dBug( $page ); die();
+
+            return $page;
+        }
+    }
--- includes/localizations/en-uk.php
+++ includes/localizations/en-uk.php
@@ -1,3 +1,3 @@
 <?php
-	//Install
-	$localization['install']['config_exists'] = "<h2>Arcfolder will not install if config.php is detected. Aborting.</h2>";
+    //Install
+    $localization['install']['config_exists'] = "<h2>Arcfolder will not install if config.php is detected. Aborting.</h2>";
