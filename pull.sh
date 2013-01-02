ebab4117ee3291c05bf51d3989a8b2b95c95b11b
Jekotia <jekotia@jekotia.net>
2013-01-02 17:13:44 -0500

    Updated pull.sh for changes to repo names (added 'PHP-' prefix to Auth and Common repo's to prevent potential future conflicts with similar libraries for other languages.

--- pull.sh
+++ pull.sh
@@ -11,14 +11,14 @@ fi
 
 
 if [ ! -d ./includes/lib/auth/ ]; then
-    git clone git://github.com/Binary-Heartbeat/Auth.git ./includes/lib/auth/
+    git clone git://github.com/Binary-Heartbeat/PHP-Auth.git ./includes/lib/auth/
 else
     cd ./includes/lib/auth/ && git reset --hard && git clean -f -d && git pull && cd ../../../
 fi
 
 
 if [ ! -d ./includes/lib/common/ ]; then
-    git clone git://github.com/Binary-Heartbeat/Common.git ./includes/lib/common/
+    git clone git://github.com/Binary-Heartbeat/PHP-Common.git ./includes/lib/common/
 else
     cd ./includes/lib/common/ && git reset --hard && git clean -f -d && git pull
 fi
